6a613b999eef6d3454b7dfa6f247e60c087d8316
Indunil Udayangana <Indunil.Udayangana@cambio.lk>
2026-05-01 19:18:56 +0530

    update vite configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 5173,
+    allowedHosts: [
+      "alb1-322075498.us-east-1.elb.amazonaws.com"
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
